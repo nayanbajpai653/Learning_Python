7ab4e62f701ca9eda674918be1632c6c584fae0f
nayanbajpai653 <nayanbajpai653@gmail.com>
2023-06-15 20:46:07 +0530

    my first commit

---
+++ Ex3.py
@@ -0,0 +1,32 @@
+print("hello world") #after debuging it print hello world
+
+print(2 ** 8) #2^8 or 2x2 eight times
+print("spam!" * 8) #we can multiply strings also 
+
+# x #x is not define so it gives error 
+
+import os #import is a module and os is a function
+os.getcwd() #it gives the location of file in system 
+
+for x in 'spam': #when it debug it print's every word in spam in different line 
+    print(x) #after debug a loop it print's single word or num at a time 
+
+print(open('my.py').read()) #it open's given file and execute it 
+
+a = 'dead'   #define three attributes
+b = 'parrot'  
+c = 'sketch'
+print(a, b, c)
+
+l = [1, 3] #list with two items
+l.append(5) #adds 5 at end
+print(l)
+
+
+
+
+
+
+
+
+
