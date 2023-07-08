ea2712846ecdc17659d221abfd4980524eca4238
nayanbajpai653 <nayanbajpai653@gmail.com>
2023-07-08 20:32:03 +0530

    Storing native python objects with pickle

--- Ex8.3.py
+++ Ex8.3.py
@@ -17,20 +17,21 @@ print(open('datafile2.pkl').read())
 
 # store and parsing packed binary data files
 F = open('data.bin','wb')                      # open binary output file
-import struct
-data = struct.pack('>i4sh' , 7 , 'spam' , 8) # make packed binary data 
+from struct import *
+data = pack('>bhl', 1, 2, 3)             # make packed binary data
 print(data)
 
 F.write(data)
 F.close()
 print(F)
 
+from struct import *
+values = unpack('>bhl',data)           # convert to python objects
+print(values)
+
 F = open('data.bin','rb')
 data = F.read                                  # get packed binary data
 print(data)
-
-values = struct.unpack('>i4sh',data)           # convert to python objects
-print(values)
 
 # object flexibility
 L = ['abc', [(1, 2), ([3], 4)], 5]
