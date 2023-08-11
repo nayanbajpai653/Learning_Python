f0227d6d37ef0085b96c076f9cf7a7ced6d06159
nayanbajpai653 <nayanbajpai653@gmail.com>
2023-08-11 20:07:24 +0530

    PyDoc: The help fuction

---
+++ Ex.13.2.py
@@ -0,0 +1,16 @@
+# PyDoc: The help fuction
+import sys
+print(help(sys.getrefcount))
+
+print(help(sys))
+
+print(help(dict))
+
+print(help(str.replace))
+
+print(help(ord))
+
+print(help(__doc__.square))
+
+
+
